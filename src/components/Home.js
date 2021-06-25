04cfd48bcbdc0030a5788504e2fc0e15cedf1e7d
ceddah <cedanik1@gmail.com>
2021-06-25 22:18:42 +0200

    fixed image urls

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useGlobalContext} from '../context'; 
 //For items on sale I can sort items by discount and splice for first 3 items and display them
 const Home = () => {
+    const {shopItems} = useGlobalContext();
+    const bestDeals = shopItems
+        .sort((a,b) => a.discount > b.discount ? -1 : 1)
+        .splice(0, 3);
+    console.log(bestDeals)
+
+    const renderItems = () => {
+        return bestDeals.map(item => (
+            <article className="deal-item">
+                <img src={item.image}  alt={item.title} />
+            </article>
+        ))
+    }
+
     return (
         <div className="home">
+            <img src="https://i.postimg.cc/sgZGN8Ch/Mom-High-Pleated-Jeans.jpg" alt="123"/>
             <div className="heading">
                 <h2>Season Sale</h2>
                 <Link to="/shop">Shop Now</Link>
             </div>
             <section className="deals">
                 <h1 className="deals__title">Deals Of The Day</h1>
+                <main className="deals-container">
+                    {renderItems()}
+                </main>
             </section>
             <div className="shop-at_image">
                 <h3>Shop at 23Threads.</h3>
--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const shop = [
         description: 'Integer tincidunt ullamcorper ex, eu dignissim risus tincidunt id. Vestibulum maximus arcu ut massa scelerisque iaculis id id leo. Praesent aliquam, dolor nec fermentum fermentum, velit turpis hendrerit ipsum, at ultrices libero turpis in orci. Nullam libero ex, lacinia sit amet turpis in, placerat vulputate risus.',
         discount: 10,
         rating: 5,
-        image: './Embrace High Ankle Jeans.jpeg'
+        image: 'https://i.postimg.cc/fTVVVFLr/Embrace-High-Ankle-Jeans.jpg'
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ const shop = [
         description: 'Nulla sit amet neque interdum, vulputate sapien non, sollicitudin felis. Ut sit amet auctor augue, at vulputate mauris. Nam ullamcorper libero sit amet enim blandit sagittis. Sed ullamcorper augue sit amet lacus efficitur, non maximus elit accumsan. Pellentesque posuere nibh magna, ac ornare mauris gravida in. Phasellus pharetra magna eget vulputate condimentum.',
         discount: 12,
         rating: 5,
-        image: './Loose High Jeans.jpeg'
+        image: 'https://i.postimg.cc/bvhrFwR7/Loose-High-Jeans.jpg'
     },
     {
         id: 3,
@@ -25,9 +25,9 @@ const shop = [
         price: 24.99,
         amount: 1,
         description: 'Maecenas nunc urna, sodales eu semper vitae, facilisis luctus justo. Etiam tempus faucibus diam, quis porta nunc finibus finibus. Donec vestibulum, dolor id convallis posuere, mauris leo eleifend mi, quis dignissim lectus massa id lectus. Phasellus vitae efficitur ipsum. Suspendisse rutrum lacinia tortor, non hendrerit lorem sodales eu. Maecenas lobortis ante non turpis congue malesuada.',
-        discount: 10,
+        discount: 25,
         rating: 4,
-        image: './Skinny Regular Jeans.jpeg'
+        image: 'https://i.postimg.cc/BvBbWfYX/Skinny-Regular-Jeans.jpg'
     },
     {
         id: 4,
@@ -37,7 +37,7 @@ const shop = [
         description: 'Sed ullamcorper augue sit amet lacus efficitur, non maximus elit accumsan. Pellentesque posuere nibh magna, ac ornare mauris gravida in. Phasellus pharetra magna eget vulputate condimentum. Donec a lectus sit amet arcu sollicitudin eleifend ut non purus. Nam et tempus nisl, eu imperdiet nunc.',
         discount: 0,
         rating: 5,
-        image: './Mom High Pleated Jeans.jpeg'
+        image: 'https://i.postimg.cc/sgZGN8Ch/Mom-High-Pleated-Jeans.jpg'
     },
     {
         id: 5,
@@ -47,7 +47,7 @@ const shop = [
         description: 'Aliquam faucibus sapien nulla, vel malesuada diam volutpat sit amet. Aenean diam erat, facilisis vel placerat nec, lacinia eget urna. Phasellus leo eros, sodales sed tellus quis, efficitur viverra quam. Nam dolor mauris, ultrices eget imperdiet nec, vulputate et nulla. Curabitur a mauris orci. Nullam facilisis elit ipsum, vel mattis eros facilisis ut.',
         discount: 15,
         rating: 3,
-        image: './Mom High Ankle Jeans.jpeg'
+        image: 'https://i.postimg.cc/pL8yZFKQ/Mom-High-Ankle-Jeans.jpg'
     },
     {
         id: 6,
@@ -57,7 +57,7 @@ const shop = [
         description: 'Cras nulla turpis, luctus eget fermentum vitae, malesuada eget ipsum. Mauris et eros non erat mattis auctor. Suspendisse suscipit fringilla neque. In risus nibh, ullamcorper eget nibh eget, porta ultrices sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
         discount: 12,
         rating: 5,
-        image: './Skinny High Jeans.jpeg'
+        image: 'https://i.postimg.cc/QxvH1bKx/Skinny-High-Jeans.jpg'
     },
     {
         id: 7,
@@ -67,7 +67,7 @@ const shop = [
         description: 'Aliquam faucibus sapien nulla, vel malesuada diam volutpat sit amet. Aenean diam erat, facilisis vel placerat nec, lacinia eget urna. Phasellus leo eros, sodales sed tellus quis, efficitur viverra quam. Nam dolor mauris, ultrices eget imperdiet nec, vulputate et nulla. Curabitur a mauris orci. Nullam facilisis elit ipsum, vel mattis eros facilisis ut.',
         discount: 10,
         rating: 4,
-        image: './Straight High Ankle Jeans.jpeg'
+        image: 'https://i.postimg.cc/qqzvChk3/Straight-High-Split-Jeans.jpg'
     },
     {
         id: 8,
@@ -77,7 +77,7 @@ const shop = [
         description: 'Aliquam faucibus sapien nulla, vel malesuada diam volutpat sit amet. Aenean diam erat, facilisis vel placerat nec, lacinia eget urna. Phasellus leo eros, sodales sed tellus quis, efficitur viverra quam. Nam dolor mauris, ultrices eget imperdiet nec, vulputate et nulla. Curabitur a mauris orci. Nullam facilisis elit ipsum, vel mattis eros facilisis ut.',
         discount: 0,
         rating: 5,
-        image: './Straight High Split Jeans.jpeg'
+        image: 'https://i.postimg.cc/qqzvChk3/Straight-High-Split-Jeans.jpg'
     },
     {
         id: 9,
@@ -87,7 +87,7 @@ const shop = [
         description: 'Fusce in commodo lacus, sit amet semper libero. Suspendisse potenti. Nam accumsan volutpat scelerisque. Morbi ligula arcu, porttitor sit amet risus eget, tincidunt malesuada justo. Aliquam non nibh scelerisque, blandit quam iaculis, cursus libero. Quisque at sagittis lacus. Curabitur elementum tempor placerat.',
         discount: 0,
         rating: 4,
-        image: './Super Skinny High Jeans.jpeg'
+        image: 'https://i.postimg.cc/9MxfMtj2/Super-Skinny-High-Jeans.jpg'
     },
     {
         id: 10,
@@ -97,7 +97,7 @@ const shop = [
         description: 'consectetur adipiscing elit. Sed rhoncus ac metus in porta. Sed maximus sapien id gravida commodo. Nam bibendum, metus eget ultrices posuere, purus lacus interdum lorem, sed pharetra ligula ipsum vel nisl. Aenean urna urna, tincidunt quis semper eu, pharetra sit amet erat. Integer elementum et augue vitae convallis.',
         discount: 30,
         rating: 3,
-        image: './Wide High Jeans.jpeg'
+        image: 'https://i.postimg.cc/yd6dsptn/Wide-High-Jeans.jpg'
     },
 ]
 
